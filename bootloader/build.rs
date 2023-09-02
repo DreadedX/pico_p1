831f180046d50ba7797975ff8832e5b5456c6565
Dreaded_X <tim@huizinga.dev>
2023-09-02 05:28:51 +0200

    Added third stage bootloader

--- bootloader/build.rs
+++ bootloader/build.rs
@@ -12,31 +12,25 @@ fn main() {
     File::create(out.join("memory.x")).unwrap();
 
     // Use memory.x.in as a template for the actual memory.x
-    let memory = include_str!("memory.x.in")
-        .replace("{BOOTLOADER}", "BOOTLOADER")
-        .replace("{ACTIVE}", "FLASH");
+    let memory = include_str!("../memory.x.in")
+        .replace("{BOOTLOADER}", "FLASH")
+        .replace("{ACTIVE}", "ACTIVE");
 
     // And then include it with a unique name
-    File::create(out.join("memory_app.x"))
+    File::create(out.join("memory_boot.x"))
         .unwrap()
         .write_all(memory.as_bytes())
         .unwrap();
     println!("cargo:rustc-link-search={}", out.display());
-    println!("cargo:rerun-if-changed=memory.x.in");
+    println!("cargo:rerun-if-changed=../memory.x.in");
 
     // And link with that one
-    println!("cargo:rustc-link-arg-bins=-Tmemory_app.x");
+    println!("cargo:rustc-link-arg-bins=-Tmemory_boot.x");
 
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
-    println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
-
-    if let Ok(dotenv_path) = dotenvy::dotenv() {
-        println!("cargo:rerun-if-changed={}", dotenv_path.display());
-
-        for env_var in dotenvy::dotenv_iter().unwrap() {
-            let (key, value) = env_var.unwrap();
-            println!("cargo:rustc-env={key}={value}");
-        }
+    println!("cargo:rustc-link-arg-bins=-Tlink-rp.x");
+    if env::var("CARGO_FEATURE_DEFMT").is_ok() {
+        println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
     }
 }
