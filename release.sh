98b81ae27480fae631f92793cbf4b6bca9ceabaa
Dreaded_X <tim@huizinga.dev>
2023-09-13 01:51:30 +0200

    Include the wireless firmware in the release builds

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p target/firmware
-cargo objcopy --release --features=exclude_firmwares -- -O binary target/firmware/firmware
+cargo objcopy --release -- -O binary target/firmware/firmware
 shasum -a 512 -b target/firmware/firmware | dd ibs=128 count=1 | xxd -p -r > target/firmware/checksum
 signify -S -m target/firmware/checksum -s ~/Projects/crypt/R0/private/keys/firmware/pico_p1.sec -x target/firmware/checksum.sig
 tail -n1 target/firmware/checksum.sig | base64 -d -i | dd ibs=10 skip=1 > target/firmware/signed
